dd8c357172790e6092ccd9ef3a1f9bda5ad1462c
d.semakin <89169812633@mail.ru>
2022-08-18 03:10:28 +0300

    add Example 007 Magic (does't work)

---
+++ Example007_Magic/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+int xa=40, ya=1,
+    xb=1, yb=30,
+    xc=30, yc=30;
+
+Console.SetCursorPosition(xa, ya);
+Console.Write("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.Write("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.Write("+");
